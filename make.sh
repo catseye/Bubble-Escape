fd6ec409fc5c191b1a044ca6d73ecdd07f153549
Cat's Eye Technologies <cpressey@gmail.com>
2012-02-29 17:22:44 -0600

    Instructions on building Bubble Escape 8K.

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# To make Bubble Escape 2K:
 p65 -v -t "src/bubble escape 2k.p65" "bin/be2k.prg"
 # now start Cruncher AB+
 # Use 'A' and '-' to select AND #$00 for the de-pack effect, then
@@ -7,3 +9,8 @@ p65 -v -t "src/bubble escape 2k.p65" "bin/be2k.prg"
 # Savename: bubble escape 2k.prg
 # Start JMP $C000
 # Wait... then hit space to save
+
+# To make Bubble Escape 8K:
+# p65 "src/bubble escape 8k.p65" be8k.bin
+# And then select "Attach generic 8K cartridge image" in VICE, and
+# select be8k.bin
